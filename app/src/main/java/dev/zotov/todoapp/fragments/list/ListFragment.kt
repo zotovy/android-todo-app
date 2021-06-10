0c521945f194a3125636691f42f2c24c69413504
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-06-10 12:24:19 +0500

    create menu

--- app/src/main/java/dev/zotov/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/dev/zotov/todoapp/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package dev.zotov.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import dev.zotov.todoapp.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -27,6 +25,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set menu
+        setHasOptionsMenu(true)
+
         return view;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
