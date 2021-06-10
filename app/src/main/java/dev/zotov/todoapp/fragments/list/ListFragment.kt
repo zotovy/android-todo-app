9b6e0fee8d64893a110e51b6c86cdc9385091f4e
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-06-10 11:30:05 +0500

    navigate to AddFragment from ListFragment

--- app/src/main/java/dev/zotov/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/dev/zotov/todoapp/fragments/list/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import dev.zotov.todoapp.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
 
@@ -13,7 +15,13 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // handler floating action button on click
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment);
+        }
+
+        return view;
     }
 }
