9a0b8318d5c94e17fccf89b2015c14c0c3828060
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-06-11 18:46:16 +0500

    Create Converter

---
+++ app/src/main/java/dev/zotov/todoapp/data/Converter.kt
@@ -0,0 +1,17 @@
+package dev.zotov.todoapp.data
+
+import androidx.room.TypeConverter
+
+class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name;
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority);
+    }
+
+}
