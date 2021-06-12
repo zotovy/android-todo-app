730306fd0f2df4b73d977fbbe25f94eeb7b09334
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-06-12 12:41:56 +0500

    migrate to jetpack compose

--- app/src/main/java/dev/zotov/todoapp/MainActivity.kt
+++ app/src/main/java/dev/zotov/todoapp/MainActivity.kt
@@ -2,19 +2,30 @@ package dev.zotov.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment));
+        setContent {
+            Layout()
+        }
+
+//        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment));
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val controller = findNavController(R.id.nav_host_fragment);
         return controller.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    @Composable
+    fun Layout() {
+        Text("Hello mates")
     }
 }
